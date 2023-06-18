73d686e279fcfd6e0bd9ad70a91a42b7308d5456
QWOFTQ <112448910+QWOFTQ@users.noreply.github.com>
2023-06-19 00:05:31 +0900

    Update usermiddleware.js

--- middleware/usermiddleware.js
+++ middleware/usermiddleware.js
@@ -37,7 +37,7 @@ exports.join = (req,res)=>{     // 회원가입
     const userdata = req.body     // userid, userpw, username 데이터  body에 저장
     userlist.push(userdata)
     console.log(userdata)       // 콘솔에 입력받은 userdata 값 출력
-    res.send(alertmove(`/`,`회원가입이 완료되었습니다.`))     // 회원가입이 완료되었습니가 알림 후 메인 페이지 이동
+    res.send(alertmove(`/`,`회원가입이 완료되었습니다.`))     // 회원가입이 완료되었습니다 알림 후 메인 페이지 이동
 }
 
 exports.create = (req,res)=>{     // 회원가입을 진행할 경우 user/join 페이지 보여주기 
@@ -46,7 +46,7 @@ exports.create = (req,res)=>{     // 회원가입을 진행할 경우 user/join 
 
 exports.quit = (req,res)=>{   // 회원 탈퇴
     const {user} = req.session    // 세션에 저장되어 있는 user 정보
-    userlist = userlist.filter((v) => v.userid !== user.userid);    // label에 있는 유저값과 세션에 저장되어있는 유저값이 같은경우
+    userlist = userlist.filter((v) => v.userid !== user.userid);    // label에 있는 유저값과 세션에 저장되어 있는 유저값이 같은경우
     res.send(alertmove(`/user/logout`,`회원탈퇴가 완료되었습니다.`))      // 회원탈퇴가 완료되었습니다 알림 후 logut
 }
 
