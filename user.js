e08432149f8ecf36a73fb4e18e292b4a107923b3
QWOFTQ <112448910+QWOFTQ@users.noreply.github.com>
2023-06-18 23:58:15 +0900

    Delete user.js

--- user.js
+++
@@ -1,7 +0,0 @@
-let user = [        //user 정보 임시 데이터,  별도의 회원가입 과정 없이 바로 로그인 가능
-  { userid: `admin`, userpw: `1234`, username: `adm` },
-  { userid: `idh1234`, userpw: `123456`, username: `1` },
-  { userid: `ihj`, userpw: `12345678`, username: `2` },
-];
-
-module.exports = user;
